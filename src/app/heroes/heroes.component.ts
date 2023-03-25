1fcd1d8363fd4d98d315a77f10383770a61fa771
RAnderson96 <rory.cameron.anderson@gmail.com>
2023-03-25 18:57:38 +0000

    reworked code to allow for asynchronous operations via Observables and Subscribe RxJS methods.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -9,11 +11,19 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent {
 
-
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+  constructor( private heroService: HeroService){ }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 
 
